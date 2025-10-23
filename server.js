ca104abcc330920645d003ca450abea81f6c0d69
HumaGitGud <khomidov.huma@student.greenriver.edu>
2025-10-23 12:18:13 -0700

    server edits

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import express from 'express';
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
+const HOST = '0.0.0.0'; // for docker to listen on 0.0.0.0 and not PC localhost
 
 app.get('/', (req, res) => {
   res.send('<html><body style="background-color: black; color: white;">Hello World!</body></html>');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server started on http://localhost:${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server started on http://${HOST}:${PORT}`);
 });
